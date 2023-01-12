b6d7aee75ba49dbec59391239aa6762890f801de
Paulo Matos <paulojomatos@gmail.com>
2023-01-12 20:43:41 -0300

    first commit

---
+++ Animal.cs
@@ -0,0 +1,21 @@
+namespace HelloWorld
+{
+    class Animal
+    {
+        private string name;
+
+        private int age { get; set; }
+
+        public Animal(string name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void info()
+        {
+            Console.WriteLine("Name is " + this.name);
+            Console.WriteLine("Age is " + this.age);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal("dog", 12);
+            animal1.info();
+        }
+    }
+}
